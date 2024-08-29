4545e52ef9490952ba70ef2fac17123ac6d93343
Suraj Prajapati <surajprajapati8286@gmail.com>
2024-08-29 21:44:10 +0530

    firebase-crashlytics on Elevated btn with throw Exception

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
 import 'package:elearn/analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  // Initialize Crashlytics
+  // Log non-fatal errors to Crashlytics
+  FlutterError.onError = (FlutterErrorDetails errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics (for Native Platform)
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance
+        .recordError(error, stack, fatal: true, reason: 'Test #101');
+    return true;
+  };
   runApp(const MyApp());
 }
 
@@ -24,7 +37,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Analytics'),
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
     );
   }
 }
@@ -47,6 +60,11 @@ class _MyHomePageState extends State<MyHomePage> {
   //   });
   // }
 
+  void triggerCrash() {
+    // FirebaseCrashlytics.instance.log('This is Test Crash!');
+    throw Exception('This is a test crash for Firebase Crashlytics. #404');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,6 +82,11 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            ElevatedButton(
+              // for C R A S H L Y T I C S
+              onPressed: triggerCrash,
+              child: const Text('Trigger crash'),
             ),
           ],
         ),
